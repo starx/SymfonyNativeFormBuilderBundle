2af0ce5cb0fb9e78709f56b2d73461fbb0434426
Nabin Nepal <n30574rx@gmail.com>
2020-09-10 20:13:08 +0100

    Fixes deprecation error

--- DependencyInjection/StarxSymfonyNativeFormBuilderExtension.php
+++ DependencyInjection/StarxSymfonyNativeFormBuilderExtension.php
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * @link http://symfony.com/doc/current/cookbook/bundles/extension.html
  */
-class StarxSymfonyNativeFormBuilderExtension extends Extension
+class StarxSymfonyNativeFormBuilderExtension extends Extension implements \Twig_Extension_GlobalsInterface
 {
     /**
      * {@inheritdoc}
